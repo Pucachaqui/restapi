77839ab8881e334f8431d4d7faf4694352f65722
Pucachaqui <151644441+Pucachaqui@users.noreply.github.com>
2024-01-08 15:39:35 -0500

    base

--- src/config.js
+++ src/config.js
@@ -4,8 +4,8 @@ config();
 console.log(process.env.PASSWORD);
 
 export default{
-    host: process.env.HOST ||"",
-    database: process.env.DATABASE ||"",
-    user: process.env.US || "root",
-    password: process.env.PASSWORD||""
+    host: "viaduct.proxy.rlwy.net",
+    database: "railway",
+    user:"root",
+    password: "3F45egA-4dGED5eE5c3H4dEbDB4E4fEH"
 };
